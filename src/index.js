1f8c0d3ab67a148ad2c48bfbb4d712c317aa014b
sushil dahal <susildahal234@gmail.com>
2025-12-21 16:35:04 +0545

    add a user url

--- src/index.js
+++ src/index.js
@@ -34,10 +34,10 @@ app.use(cookieParser()); // Added cookie-parser middleware
 
 
 const corsOptions = {
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://dairy-frontend-eight.vercel.app/"],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true
-}
+};
 
 app.use(cors(corsOptions));
 
@@ -59,7 +59,7 @@ app.use(errorHandlerMiddleware);
 // Start server
 mongodbConnection()
 .then(() => {
-    app.listen(port, () => {
+    app.listen(port, "0.0.0.0", () => {
         console.log(`Server is running on port ${port}`);
     });
 })
